376e3e38cfe6689f692523e746c3d65937b94541
Kyle Haefner <khaefner@gmail.com>
2021-04-01 16:18:57 -0600

    mutex tasks

--- src/main.c
+++ src/main.c
@@ -6,79 +6,58 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
 
 
 #define PIN 10   //M5stick built in LED
 
+xSemaphoreHandle mutexData;
 
-static TaskHandle_t rHandler = NULL;
-
-void countByTwo(void * params)
+void protectedWrite(char *message)
 {
-    int counter = 0;
-    while (true)
-    {
-        /* code */
-      counter=counter+2;
-      printf("The current 2 count is %d\n",counter);
-      vTaskDelay(2000/portTICK_PERIOD_MS);
-    }
-    
+    printf(message);
 }
 
-void countByThree(void * params)
-{
-    int counter = 0;
-    while (true)
-    {
-        /* code */
-     counter = counter +3;
-     printf("The current 3 count is %d\n",counter);   
-     vTaskDelay(3000/portTICK_PERIOD_MS);
-    }
-    
-}
-
-void blinkLed()
-{
-    gpio_pad_select_gpio(PIN);
-    gpio_set_direction(PIN,GPIO_MODE_OUTPUT); //Tell the device to set this pin as output
-    int ledOn = 0;
-    while(1)
-    {
-        ledOn = !ledOn;
-        gpio_set_level(PIN, ledOn);
-        vTaskDelay(1000/portTICK_PERIOD_MS);
-    }
-}
-
-
-void sender(void * params)
+void task1(void * params)
 {
     while (true)
     {
-        xTaskNotify(rHandler, /*THe task handler*/
-                    1,   /*data we are sending*/
-                    eSetValueWithOverwrite); /*Notify Action  this one overwrites the data*/
+        if(xSemaphoreTake(mutexData,1000/portTICK_PERIOD_MS))
+        {
+            protectedWrite("I'm task 1\n");
+            xSemaphoreGive(mutexData);
+        }
+        else
+        {
+            printf("Could not get protected write\n");
+        }
         vTaskDelay(1000/portTICK_PERIOD_MS);
     }
     
+
 }
 
-void receiver(void * params)
+void task2(void * params)
 {
-    uint data;
     while (true)
     {
-        xTaskNotifyWait(0,0,&data,portMAX_DELAY);
-        printf("Received:  %d\n",data);
+        if(xSemaphoreTake(mutexData,1000/portTICK_PERIOD_MS))
+        {
+            protectedWrite("I'm task 2\n");
+            xSemaphoreGive(mutexData);
+        }
+        else
+        {
+            printf("Task2: Could not get protected write\n");
+        }
+        vTaskDelay(2000/portTICK_PERIOD_MS);
     }
-    
 }
 
 
 void app_main(void) 
 {
-    xTaskCreate(&receiver,"reciever",2048,NULL,2,&rHandler);
-    xTaskCreate(&sender,"sender",2048,NULL,2,NULL);
+    mutexData = xSemaphoreCreateMutex();
+    xTaskCreate(&task1,"Task 1",2048,NULL,1,NULL);
+    xTaskCreate(&task2,"Task 2",2048,NULL,1,NULL);
 }
