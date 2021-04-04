a1899f68ac8e83c3ec1bcaaf919d629a4454fbc0
Kyle Haefner <khaefner@gmail.com>
2021-04-04 15:40:21 -0600

    Queues with producer and Consumer

--- src/main.c
+++ src/main.c
@@ -6,32 +6,44 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
+
 
 
 #define PIN 10   //M5stick built in LED
 
-void countByTwo(void * params)
+xQueueHandle queue;  /*declare queue handle*/
+
+void taskProducer(void * params)
 {
+    /*Producer puts up to three items on the queue*/
     int counter = 0;
     while (true)
     {
         /* code */
-      counter=counter+2;
-      printf("The current 2 count is %d\n",counter);
+      counter++;
+      printf("The producer counter is %d\n",counter);
+      long success = xQueueSend(queue,&counter,1000/portTICK_PERIOD_MS); /*send to hue handle the address of the counter and timeout of 1000 ms* return success*/
+      if(success)
+      {
+          printf("Successfully added item to the queue\n");
+      }
+      else
+      {
+          printf("failed to add Item to the queue\n");
+      }
       vTaskDelay(2000/portTICK_PERIOD_MS);
     }
     
 }
 
-void countByThree(void * params)
+void taskConsumer(void * params)
 {
-    int counter = 0;
+    int receivedInt = 0;
     while (true)
     {
-        /* code */
-     counter = counter +3;
-     printf("The current 3 count is %d\n",counter);   
-     vTaskDelay(3000/portTICK_PERIOD_MS);
+     xQueueReceive(queue, &receivedInt,5000/portTICK_PERIOD_MS);   /*queue we are receiving from, the address fo memory tto store in, size of what to store*/
+     printf("Consumer got %d from queue\n",receivedInt);   
     }
     
 }
@@ -52,12 +64,13 @@ void blinkLed()
 
 
 void app_main(void) {
-    xTaskCreate( &countByTwo, /* Pointer to the function that implements the task. */
+    queue = xQueueCreate(3,sizeof(int));   /* three items on the queueu of size integer*/
+    xTaskCreate( &taskProducer, /* Pointer to the function that implements the task. */
                 "Count by two",/* Text name for the task. This is to facilitate debugging only. */
                 2048, /* Stack depth - how much memory to use for this task*/
                 NULL, /* This example does not use the task parameter. */
                 1, /* This task will run at priority 1.  Higher is more higher priority */
                 NULL ); /* Task Handler to refer to task later. This example does not use the task handle. */
-    xTaskCreate(&countByThree,"Count By three",2048, NULL,2,NULL);  /*HThis is the task call on one line */
+    xTaskCreate(&taskConsumer,"Count By three",2048, NULL,2,NULL);  /*HThis is the task call on one line */
     xTaskCreatePinnedToCore(&blinkLed,"Blinky blink",1000,NULL,3,NULL,1); /* Blink the LED highest priority pinned to second core */
 }
